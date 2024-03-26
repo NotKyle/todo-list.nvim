41f4f8961d0cd055a800fb6458ae67b1c9c5aff4
Kyle Russell <NotKyle@users.noreply.github.com>
2024-03-26 15:58:44 +0000

    Updates

--- lua/todo-list/utils.lua
+++ lua/todo-list/utils.lua
@@ -65,6 +65,40 @@ function M.list_tasks()
 		end
 	end
 
+	-- Open a floating window to display the tasks
+	-- We can use the floating window to add and remove tasks
+
+	-- Create a new buffer
+	local buf = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+	vim.api.nvim_buf_set_option(buf, "buftype", "nofile")
+	vim.api.nvim_buf_set_option(buf, "swapfile", false)
+	vim.api.nvim_buf_set_option(buf, "buflisted", false)
+	vim.api.nvim_buf_set_option(buf, "filetype", "todo")
+
+	-- Set the buffer content
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, tasks)
+
+	-- Create a new window
+	local width = vim.api.nvim_get_option("columns")
+	local height = vim.api.nvim_get_option("lines")
+	local win_height = math.ceil(height * 0.8 - 4)
+	local win_width = math.ceil(width * 0.8)
+	local row = math.ceil((height - win_height) / 2 - 1)
+	local col = math.ceil((width - win_width) / 2)
+	local opts = {
+		style = "minimal",
+		relative = "editor",
+		width = win_width,
+		height = win_height,
+		row = row,
+		col = col,
+	}
+
+	local win = vim.api.nvim_open_win(buf, true, opts)
+	vim.api.nvim_win_set_option(win, "winblend", 0)
+
+	-- Close the file
 	file:close()
 
 	return tasks
