88cb07dec06948be78c46fb796cda47bed58898a
Kyle Russell <NotKyle@users.noreply.github.com>
2024-03-26 15:09:50 +0000

    Changes

--- lua/init.lua
+++ lua/init.lua
@@ -1,103 +1,59 @@
-local fn = vim.fn
-
 local M = {}
 
-local function create_todo_file()
-	local project_name = fn.fnamemodify(fn.getcwd(), ":t")
-	local todo_file = fn.getcwd() .. "/.todo"
-	local todo_file_exists = fn.filereadable(todo_file) == 1
+-- Define the file name to save todo lists
+local TODO_FILE_NAME = "todo_list.json"
 
-	if not todo_file_exists then
-		local file = io.open(todo_file, "w")
-		file:write(project_name .. "\n")
+-- Function to load todo list from file
+local function load_todo_list(project_path)
+	local file_path = project_path .. "/" .. TODO_FILE_NAME
+	local file = io.open(file_path, "r")
+	if file then
+		local content = file:read("*a")
+		file:close()
+		return vim.fn.json_decode(content)
+	else
+		return {}
+	end
+end
+
+-- Function to save todo list to file
+local function save_todo_list(project_path, todo_list)
+	local file_path = project_path .. "/" .. TODO_FILE_NAME
+	local file = io.open(file_path, "w")
+	if file then
+		file:write(vim.fn.json_encode(todo_list))
 		file:close()
 	end
 end
 
-local function get_todo_list()
-	local todo_file = fn.getcwd() .. "/.todo"
-	local todo_file_exists = fn.filereadable(todo_file) == 1
-
-	if not todo_file_exists then
-		return {}
-	end
-
-	local todo_list = {}
-	for line in io.lines(todo_file) do
-		table.insert(todo_list, line)
-	end
-
-	return todo_list
+-- Function to add a task to todo list
+function M.add_task(project_path, task, priority)
+	local todo_list = load_todo_list(project_path)
+	table.insert(todo_list, { task = task, priority = priority })
+	save_todo_list(project_path, todo_list)
 end
 
-local function add_todo()
-	local project_name = fn.fnamemodify(fn.getcwd(), ":t")
-	local todo_file = fn.getcwd() .. "/.todo"
-	local todo_file_exists = fn.filereadable(todo_file) == 1
-
-	if not todo_file_exists then
-		create_todo_file()
-	end
-
-	local task = fn.input("Task: ")
-	local priority = fn.input("Priority (1-5): ")
-
-	local file = io.open(todo_file, "a")
-	file:write(task .. " " .. priority .. "\n")
-	file:close()
+-- Function to remove a task from todo list
+function M.remove_task(project_path, index)
+	local todo_list = load_todo_list(project_path)
+	table.remove(todo_list, index)
+	save_todo_list(project_path, todo_list)
 end
 
-local function remove_todo()
-	local project_name = fn.fnamemodify(fn.getcwd(), ":t")
-	local todo_file = fn.getcwd() .. "/.todo"
-	local todo_file_exists = fn.filereadable(todo_file) == 1
-
-	if not todo_file_exists then
-		return
-	end
-
-	local todo_list = get_todo_list()
-	local task = fn.input("Task to remove: ")
-
-	local file = io.open(todo_file, "w")
-	for _, line in ipairs(todo_list) do
-		if not string.match(line, task) then
-			file:write(line .. "\n")
-		end
-	end
-	file:close()
-end
-
-local function list_todo()
-	local todo_list = get_todo_list()
-
-	for _, line in ipairs(todo_list) do
-		print(line)
+-- Function to list tasks in todo list
+function M.list_tasks(project_path)
+	local todo_list = load_todo_list(project_path)
+	for i, task in ipairs(todo_list) do
+		print(i .. ". " .. task.task .. " (Priority: " .. task.priority .. ")")
 	end
 end
 
-function M.todo()
-	local todo_commands = {
-		add = add_todo,
-		remove = remove_todo,
-		list = list_todo,
-	}
-
-	local command = fn.input("Command (add, remove, list): ")
-	todo_commands[command]()
-end
+-- Key binding to open todo list in a floating window
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>tl",
+	'<cmd>lua require("todo").list_tasks(vim.fn.getcwd())<CR>',
+	{ noremap = true, silent = true }
+)
 
 return M
-
--- Make it compativle with LazyVim plugin manager
-
--- Path: lua/init.lua
--- Create command
-
-local todo = require("todo")
-
-vim.cmd([[command! -nargs=0 Todo lua require('todo').todo()]])
-
--- Keymap
-vim.api.nvim_set_keymap("n", "<leader>td", ":Todo<CR>", { noremap = true, silent = true })
-
